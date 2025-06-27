2b9ff275e4c0f027e9d720848296c9c19246f1ae
jenwardpauleusebio <jenwardpauleusebio@gmail.com>
2025-06-27 09:08:20 +0800

    ResourceHive

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    '../resourcehive.html', // Adjust this based on where your HTML/JS files are
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  
+};
